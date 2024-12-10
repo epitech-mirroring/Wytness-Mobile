2863ae430d9776606570c20fb0771b7129cd95d7
Antoinegtir <antoine.gonthier@epitech.eu>
2024-12-10 21:11:12 +0100

    🔐 create auth http request

---
+++ lib/service/auth.service.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:mobile/constants/const.dart';
+
+class AuthService {
+  static Future<String> login(String email, String password) async {
+    try {
+      http.Response response = await http.post(
+        Uri.parse('http://localhost:3000/auth/login'),
+        body: {
+          'email': email,
+          'password': password,
+        },
+      );
+      final data = jsonDecode(response.body);
+      if (response.statusCode == 201) {
+        localUser.setString('token', data['token']);
+        return 'token';
+      } else {
+        return data['message'] ?? response.body;
+      }
+    } catch (e) {
+      print(e);
+      return '';
+    }
+  }
+
+  static Future<String> register(
+    String email,
+    String password,
+    String name,
+    String surname,
+  ) async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://localhost:3000/auth/register'),
+        body: {
+          'email': email,
+          'password': password,
+          'name': name,
+          'surname': surname,
+        },
+      );
+      Map<String, dynamic> data = jsonDecode(response.body);
+      if (response.statusCode == 201) {
+        if (data.containsKey('token')) {
+          localUser.setString('token', data['token']);
+          return 'token';
+        } else {
+          return data['message'] ?? data.toString();
+        }
+      } else {
+        return data['message'] ?? data.toString();
+      }
+    } catch (e) {
+      print(e);
+      return '';
+    }
+  }
+}
