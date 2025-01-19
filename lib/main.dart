fef95448ce3e4c46d7c70f1c8cc4b02aba29459e
Antoinegtir <antoine.gonthier@epitech.eu>
2025-01-19 15:51:05 +0100

    📲 handle deeplink by retrieveing querys from link to connect to a service

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:app_links/app_links.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/constants/const.dart';
 import 'package:mobile/onboarding/onboard.dart';
 import 'package:mobile/pages/home.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -65,17 +68,45 @@ class _EntryPointPageState extends State<EntryPointPage> {
     });
   }
 
-  void _handleIncomingLink(Uri uri) {
-    print('Received link: $uri');
+  void _handleIncomingLink(Uri uri) async {
+    Map<String, String> queryParameters = uri.queryParameters;
+    if (queryParameters.containsKey('code') &&
+        queryParameters.containsKey('service') &&
+        queryParameters.containsKey('state')) {
+      final service = queryParameters['service'];
+      final code = queryParameters['code'];
+      final state = queryParameters['state'];
+      if (service != null && code != null && state != null) {
+        print(jsonEncode({
+          'code': code,
+          'state': state,
+        }));
+        try {
+          final data = await http.post(
+            Uri.parse('http://localhost:4040/api/services/$service/connect'),
+            headers: {
+              'Authorization':
+                  'Bearer ${await FirebaseAuth.instance.currentUser!.getIdToken()}',
+              'Content-Type': 'application/json',
+            },
+            body: jsonEncode({
+              'code': code,
+              'state': state,
+            }),
+          );
+          print(data.statusCode);
+        } catch (e) {
+          print(e);
+        }
+      }
+    }
   }
 
   Widget pageEntrypointPage() {
     if (localUser.getString('token') == null) {
       return const OnBoardPage();
     }
-    return HomePage(
-      workflow: workflowService,
-    );
+    return const HomePage();
   }
 
   @override
